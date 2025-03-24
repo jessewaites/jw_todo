93700c11e8532021a599e3883b4ef76f9864f764
Jesse Waites <1413595+jessewaites@users.noreply.github.com>
2025-03-24 13:18:57 -0400

    make user_id a hidden field in list form

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,5 @@
 class ListsController < ApplicationController
+  before_action :authenticate_user!, except: [ :index, :show ]
   before_action :set_list, only: %i[ show edit update destroy ]
 
   # GET /lists or /lists.json
